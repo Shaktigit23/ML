5a1ea5302d77ec88f2f4a90c659a789f614c38a4
Shaktiprasad Rudrawar <rudrawarshaktiprasad@gmail.com>
2024-06-15 00:48:20 +0530

    second commit

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name = "ML Project",
     version = "0.0.1",
-    auther = "Shaktiprasad Rudrawar",
+    author = "Shaktiprasad Rudrawar",
     author_email = "rudrawarshaktiprasad@gmail.com",
     packages = find_packages(),
     install_requires = get_requirements('requirements.txt'),    
